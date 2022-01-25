26593b22f89b94ab3276a91ef2f206ae7b95d730
thornbird <thornbird.blue@gmail.com>
2022-01-25 17:53:54 +0800

    ci origin code.

---
+++ src/cc/thornbird/acammonitor/ACamMonitor.kt
@@ -0,0 +1,13 @@
+package cc.thornbird.acammonitor
+
+import android.os.Bundle
+import android.app.Activity
+
+class ACamMonitor : Activity() {
+	    override fun onCreate(savedInstanceState: Bundle?) {
+	        super.onCreate(savedInstanceState)
+	        setContentView(R.layout.acammonitor_activity)
+	    }
+}
+
+
